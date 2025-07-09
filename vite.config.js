3c4488ab5fe5c0af4b65311412f2928e3cb98ae7
akashkumar491 <visitak7@gmail.com>
2025-07-09 17:46:48 +0530

    fix-2 in vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, 'src/main.jsx'),
+      input: 'src/main.jsx',
       output: {
         entryFileNames: 'widget-app.js',
       },
     },
-    cssCodeSplit: false
   },
 });
