51d0d79ea2dafb9b92906930b02a35871e3fee9a
akashkumar491 <visitak7@gmail.com>
2025-07-09 17:35:18 +0530

    changes vite configuration to inject inline css

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -5,10 +5,10 @@
   container.id = 'chat-widget-root';
   document.body.appendChild(container);
 
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'https://chat-widget-ashen.vercel.app/assets/main-DrYNO3aF.css';
-  document.head.appendChild(link);
+//   const link = document.createElement('link');
+//   link.rel = 'stylesheet';
+//   link.href = 'https://chat-widget-ashen.vercel.app/assets/main-DrYNO3aF.css';
+//   document.head.appendChild(link);
 
   const script = document.createElement('script');
   script.src = `https://chat-widget-ashen.vercel.app/widget-app.js?propertyId=${propertyId}`;
--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
         entryFileNames: 'widget-app.js',
       },
     },
+    cssCodeSplit: false
   },
 });
