33fdb9fb772c64adfea93cc096ef90b1a0c6af9a
akashkumar491 <visitak7@gmail.com>
2025-07-09 16:55:04 +0530

    added css file in script

--- public/widget-loader.js
+++ public/widget-loader.js
@@ -5,6 +5,11 @@
   container.id = 'chat-widget-root';
   document.body.appendChild(container);
 
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://chat-widget-ashen.vercel.app/assets/main-DrYNO3aF.css';
+  document.head.appendChild(link);
+
   const script = document.createElement('script');
   script.src = `https://chat-widget-ashen.vercel.app/widget-app.js?propertyId=${propertyId}`;
   script.type = 'module';
